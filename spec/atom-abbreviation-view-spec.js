a65800ad01a77fd92e8676296da768b3dce4e3ac
Charlie Fish <fishcharlie@me.com>
2016-06-21 15:35:31 -0600

    Initial Commit

---
+++ spec/atom-abbreviation-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomAbbreviationView from '../lib/atom-abbreviation-view';
+
+describe('AtomAbbreviationView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
