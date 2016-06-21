10aa1145c5c1fb6437ba2206fcc2dd200fc72b0f
Charlie Fish <fishcharlie@me.com>
2016-06-21 15:49:17 -0600

    Remove unnecessary files

--- spec/atom-abbreviation-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomAbbreviationView from '../lib/atom-abbreviation-view';
-
-describe('AtomAbbreviationView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
